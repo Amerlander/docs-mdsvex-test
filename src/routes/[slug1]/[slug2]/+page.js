6ab0994245371aae94e628e4ef77357502852d1e
JW <Amerlander@users.noreply.github.com>
2022-08-23 19:28:56 +0200

    Initial Commit

---
+++ src/routes/[slug1]/[slug2]/+page.js
@@ -0,0 +1,36 @@
+import { error } from '@sveltejs/kit';
+
+import { slugFromPath } from '$lib/util';
+import LayoutLoader from '$lib/layouts/LayoutLoader.svelte'
+
+/**
+ * @type {import('@sveltejs/kit').PageLoad}
+ */
+export async function load({ url, fetch, params }) {
+
+	let Page;
+
+	const pagesIndex = await import.meta.globEager(`../../../content/*/*/index{.,.de.,.en.}{md,svx,svelte}`);
+	const pagesFolders = await import.meta.globEager(`../../../content/*/*{.,.de.,.en.}{md,svx,svelte}`);
+	const pages = {...pagesIndex, ...pagesFolders}
+	let match;
+        for (const [path, resolver] of Object.entries(pages)) {
+            if (slugFromPath(path) === `${params.slug1}/${params.slug2}`) {
+                match = [path, resolver];
+                break;
+            }
+        }
+
+        if (!match) {
+            throw error(404);
+        }
+
+	
+    Page = match[1].default;
+	let meta = match[1].metadata ?? {}		
+
+	return {
+		Page,
+		meta
+	};
+}
